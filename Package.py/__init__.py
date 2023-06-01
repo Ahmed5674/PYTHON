d6af4187073184b2d736cd10aa9e973998ebd0f7
Ahmed5674 <ahmednayeem555@gmail.com>
2023-06-01 20:37:59 +0000

    adding package programs

---
+++ Package.py/__init__.py
@@ -0,0 +1,12 @@
+# main.py
+from my_package.class1 import Class1
+from my_package.class2 import Class2
+
+# Create objects for the classes
+obj1 = Class1()
+obj2 = Class2()
+
+# Call the methods
+obj1.method1()
+obj2.method2()
+
